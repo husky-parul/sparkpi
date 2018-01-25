2bad732806fdffc1f22e9d46817077ba576eaefb
husky-parul <promptparul@gmail.com>
2018-01-25 14:47:53 -0500

    job to add new dataset

--- app.py
+++ app.py
@@ -1,25 +1,60 @@
 import time
 
 from pyspark import SparkContext
-from pyspark.streaming import StreamingContext
+from pyspark.sql import SQLContext
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
+
+import json
+import requests 
 
 if __name__ == "__main__":
 
-    sc = SparkContext(appName="PythonStreamingQueueStream")
-    ssc = StreamingContext(sc, 1)
+    dataverse_server = 'http://128.31.24.163:8080' # no trailing slash
+    api_key = 'f86fa7e2-29be-4c00-b6d2-b83acb6e5f4b'
+    dataverse_id = "root" #database id of the dataverse 
+    url_dataverse_id = '%s/api/dataverses/%s/datasets/?key=%s' % (dataverse_server, dataverse_id, api_key)
 
-    # Create the queue through which RDDs can be pushed to
-    # a QueueInputDStream
-    rddQueue = []
-    for i in range(5):
-        rddQueue += [ssc.sparkContext.parallelize([j for j in range(1, 1001)], 10)]
+    # # --------------------------------------------------
+    # # Using a "jsonData" parameter, add description for dataset
+    # # --------------------------------------------------
+    with open('dataset.json') as dataset_json_file:
+	    file = dataset_json_file.read()
+    data_load = json.loads(file)
+    data = json.dumps(data_load)
 
-    # Create the QueueInputDStream and use it do some processing
-    inputStream = ssc.queueStream(rddQueue)
-    mappedStream = inputStream.map(lambda x: (x % 10, 1))
-    reducedStream = mappedStream.reduceByKey(lambda a, b: a + b)
-    reducedStream.pprint()
+  
 
-    ssc.start()
-    time.sleep(6)
-    ssc.stop(stopSparkContext=True, stopGraceFully=True)
+    # # -------------------
+    # # Make the request
+    # # -------------------
+    print('-' * 40)
+    print('making request: %s' % url_dataverse_id)
+    r = requests.post(url_dataverse_id, data=data)
+
+    # # -------------------
+    # # Print the response
+    # # -------------------
+    print('-' * 40)
+    print(r.status_code)
+    json_str=r.json()
+    print 'This is json_str \n',json_str
+
+
+
+    # get SparkSession this way or from SQLContext
+    #spark = SparkSession.builder.appName("PythonRestCall").getOrCreate()
+
+  
+
+    sc = SparkContext(appName="PythonRestCall")
+    sqlContext=SQLContext(sc)
+    spark=sqlContext.sparkSession
+    
+
+    path = "output.json"
+    df = spark.read.json(path)
+    df.show()
+    rdd = sc.parallelize([json_str])
+    json_df = sqlContext.read.json(rdd)
+   
